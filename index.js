43cfee02f2bc8e5947458c84b8a0e86d111ca3d6
Jovarasleo <jovarasleonavicius@gmail.com>
2021-10-14 18:19:59 +0300

    css2

--- index.js
+++ index.js
@@ -35,30 +35,6 @@ let imageContainer = [
     src: "https://images.unsplash.com/photo-1564295644023-16f14ac50b93",
     alt: "whale tale above water surface",
   },
-  {
-    src: "https://images.unsplash.com/photo-1562207520-19c0ebd8264f",
-    alt: "green mountains and blue sky",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1617191519105-d07b98b10de6",
-    alt: "blue and purple galaxy",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1441829266145-6d4bfbd38eb4",
-    alt: "blue wavy water",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1615114814213-a245ffc79e9a",
-    alt: "brown and black galaxy",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1552604660-a8c4dde15b2e",
-    alt: "person on clif in canyon",
-  },
-  {
-    src: "https://images.unsplash.com/photo-1564295644023-16f14ac50b93",
-    alt: "whale tale above water surface",
-  },
 ];
 function createEl (type, className) {
   let element = document.createElement(type);
